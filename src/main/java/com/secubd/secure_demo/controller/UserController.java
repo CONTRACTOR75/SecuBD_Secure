4889f4e9cb374ee92942e38a4e5fcc8fec9fc07a
CONTRACTOR75 <aledimathias@gmail.com>
2025-11-30 21:17:24 +0000

    Mise à jour de l'enpoint GET /me

--- src/main/java/com/secubd/secure_demo/controller/UserController.java
+++ src/main/java/com/secubd/secure_demo/controller/UserController.java
@@ -8,11 +8,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
